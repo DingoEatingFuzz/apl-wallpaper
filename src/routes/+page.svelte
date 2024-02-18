5d2581832c2bf4d0ca58a08d00ec15baf0608d1a
Michael Lange <dingoeatingfuzz@gmail.com>
2024-02-17 22:12:01 -0800

    Basic hex drawing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,13 @@
 <style>
   main, canvas {
     width: 100%;
+  }
+  
+  main {
     height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
   aside {
@@ -38,8 +44,50 @@
   }
 </style>
 
+<script>
+  // There are two grids:
+  // 1. Hexagons, defines the overall structure
+  // 2. Triangles, six within each hexagon, defines shape placement
+
+  let canvas;
+
+  const hex = (x, y, s) => {
+    // Derive points from s. Apply translation with x, y
+    const points = [];
+    for (let i = 0; i < 6; i++) {
+      const ang = (2 * Math.PI / 6) * i - Math.PI / 2;
+      points.push({ x: Math.cos(ang) * s, y: Math.sin(ang) * s });
+    }
+    return points.map(p => ({ x: p.x + x, y: p.y + y  }));
+  }
+
+  const drawPts = (ctx, pts, fill = true, stroke = true) => {
+    console.log('pts', pts);
+    ctx.beginPath();
+    ctx.moveTo(pts[0].x, pts[0].y);
+    for (let i = 1; i < pts.length; i++) {
+      ctx.lineTo(pts[i].x, pts[i].y);
+    }
+    if (fill) ctx.fill();
+    if (stroke) ctx.stroke();
+  }
+
+  $: {
+    const ctx = canvas?.getContext('2d', { colorSpace: 'display-p3' });
+    console.log('check?', ctx);
+
+    if (ctx) {
+      ctx.fillStyle="#333344";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.fillStyle = 'white';
+      ctx.strokeStyle = 'red';
+      drawPts(ctx, hex(100, 100, 20));
+    }
+  }
+</script>
+
 <main>
-  <canvas></canvas>
+  <canvas bind:this={canvas} width="3940" height="2160"></canvas>
 </main>
 <aside>
   <h1>Applications Product Line</h1>
