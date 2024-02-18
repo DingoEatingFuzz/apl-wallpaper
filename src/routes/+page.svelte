acc37a884507b2e9f43e9b93ad4cb440d55e115f
Michael Lange <dingoeatingfuzz@gmail.com>
2024-02-17 21:20:08 -0800

    base styles and markup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <style>
+    * {
+      box-sizing: border-box;
+    }
+
+    html, body, .app-wrapper {
+      padding: 0;
+      margin: 0;
+      height: 100vh;
+      width: 100%;
+      background: oklch(0.15 0.03 225.59);
+      font-family: sans-serif;
+      color: oklch(0.75 0.03 225.59 / 1);
+    }
+  </style>
+</svelte:head>
+
+<style>
+  main, canvas {
+    width: 100%;
+    height: 100%;
+  }
+
+  aside {
+    position: fixed;
+    right: 30px;
+    top: 30px;
+    background: oklch(0.32 0.05 225.14);
+    padding: 20px;
+    border: 1px solid oklch(0.45 0.03 225.59 / 1);
+    width: 300px;
+  }
+
+  h1 {
+    color: white;
+    margin-top: 0;
+  }
+</style>
+
+<main>
+  <canvas></canvas>
+</main>
+<aside>
+  <h1>Applications Product Line</h1>
+  <p>Play with sliders to your liking and export your own 5k wallpaper.</p>
+  <form>
+    <ul>
+      <li>% filled</li>
+      <li>Hex size</li>
+      <li>Vagrant</li>
+      <li>Nomad</li>
+      <li>Waypoint</li>
+      <li>Color variation</li>
+      <li>Scale</li>
+      <li>Ultrawide?</li>
+    </ul>
+    <button>Save Image</button>
+  </form>
+</aside>
